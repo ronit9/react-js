34ca0fca6545c0931bfa2170267acaf2a5595bc6
Ronit Darji <34264289+ronit9@users.noreply.github.com>
2025-05-30 08:46:58 +0530

    Add files via upload

---
+++ projects/Restaurant [pr-6]/src/App.jsx
@@ -0,0 +1,173 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { useState } from "react";
+import Home from "./components/Header";
+import Banner from "./components/Banner";
+import Add from "./components/Add";
+import { BrowserRouter, Route, Routes, Link, useNavigate } from "react-router-dom";
+import Show from "./components/Show";
+function App() {
+  const [recipes, setRecipes] = useState({ taste: [] });
+  const [vegetarian, setVegetarian] = useState(
+    localStorage.getItem("vegetarian")
+      ? JSON.parse(localStorage.getItem("vegetarian"))
+      : []
+  );
+  const [nonveg, setNonveg] = useState(
+    localStorage.getItem("nonveg")
+      ? JSON.parse(localStorage.getItem("nonveg"))
+      : []
+  );
+
+  const [allrecipes, setAllRecipes] = useState(
+    localStorage.getItem("recipes")
+      ? JSON.parse(localStorage.getItem("recipes"))
+      : []
+  );
+  const [formerrors, setFormErrors] = useState({
+    recipeName: "",
+    info: "",
+    ingredients: "",
+    price: "",
+    image: "",
+    category: "",
+    taste: [],
+  });
+  const formInput = (e) => {
+    const { name, value, type, checked } = e.target;
+    // if (type === "radio") {
+
+    // }
+    if (type === "checkbox") {
+      if (checked) {
+        setRecipes({ ...recipes, taste: [...recipes.taste, value] });
+      } else {
+        setRecipes({
+          ...recipes,
+          taste: recipes.taste.filter((item) => {
+            return item !== value;
+          }),
+        });
+      }
+    } else {
+      setRecipes({ ...recipes, [name]: value });
+    }
+  };
+
+  const foremerr = () => {
+    let chek = true;
+    const err = {
+      recipeName: "",
+      info: "",
+      ingredients: "",
+      price: "",
+      image: "",
+      critaria: "",
+      category: "",
+      taste: [],
+    };
+    if (!recipes.recipeName) {
+      err.recipeName = "Name Is Required";
+      chek = false;
+    }
+    if (!recipes.info) {
+      err.info = "Name Is Required";
+      chek = false;
+    }
+    if (!recipes.ingredients) {
+      err.ingredients = "Ingredients Is Required";
+      chek = false;
+    }
+    if (!recipes.price) {
+      err.price = "Price Is Required";
+      chek = false;
+    }
+    if (!recipes.image) {
+      err.image = "Image Is Required";
+      chek = false;
+    }
+    if (!recipes.critaria) {
+      err.critaria = "Critaria Is Required";
+      chek = false;
+    }
+    if (!recipes.category) {
+      err.category = "Category Is Required";
+      chek = false;
+    }
+    if (!recipes.taste) {
+      err.taste = "Taste Is Required";
+      chek = false;
+    }
+    setFormErrors(err);
+    return chek;
+  };
+  const formSubmit = (e) => {
+    e.preventDefault();
+    if (foremerr()) {
+      let addRecipe = {
+        id: Math.floor(Math.random() * 1000),
+        ...recipes,
+      };
+      let rec = [...allrecipes, addRecipe];
+      setAllRecipes(rec);
+      localStorage.setItem("recipes", JSON.stringify(rec));
+
+      let veg = rec.filter((item) => item.critaria === "vegetarian");
+      let nonveg = rec.filter((item) => item.critaria === "non-vegetarian");
+      let des=rec.filter((item) => item.critaria === "desert");
+      if (veg.length > 0) {
+        setVegetarian(veg);
+        localStorage.setItem("vegetarian", JSON.stringify(veg));
+      }
+
+      if (nonveg.length > 0) {
+        setNonveg(nonveg);
+        localStorage.setItem("nonveg", JSON.stringify(nonveg));
+      }
+
+      if (des.length > 0) {
+        setNonveg(des);
+        localStorage.setItem("dessert", JSON.stringify(des));
+      }
+
+      setRecipes({
+        recipeName: "",
+        info: "",
+        ingredients: "",
+        price: "",
+        image: "",
+        category: "",
+        taste: [],
+      });
+    }
+  };
+
+  useNavigate("/banner");
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="banner" element={<Banner />} />
+          <Route
+            path="add"
+            element={
+              <Add
+                formSubmit={formSubmit}
+                formInput={formInput}
+                allrecipes={allrecipes}
+                setRecipes={setRecipes}
+                formerrors={formerrors}
+                recipes={recipes}
+              />
+            }
+          />
+          <Route path="show" element={<Show allrecipes={allrecipes} />} />
+        </Routes>
+      </BrowserRouter>
+      <Banner />
+    </>
+  );
+}
+
+export default App;
---
+++ projects/Restaurant [pr-6]/src/components/Header.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { FaHome } from "react-icons/fa";
+import { PiNotepadDuotone } from "react-icons/pi";
+import { MdOutlinePriceChange } from "react-icons/md";
+import { FaClipboardList } from "react-icons/fa";
+import { MdAccountBox } from "react-icons/md";
+import Image from "react-bootstrap/Image";
+import { Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import Banner from "./Banner";
+const home = () => {
+  let location=useLocation()
+console.log(location);
+
+  return (
+    <>
+      <header>
+        <Navbar data-bs-theme="dark">
+          <Container className="d-flex align-items-center">
+            <Col lg={2}>
+              <Navbar.Brand>
+                <Image src="../public/logo.png" rounded />
+              </Navbar.Brand>
+            </Col>
+            <Col lg={9}>
+              <Nav className="d-flex justify-content-between Lobster">
+                <Link
+                  to="/"
+                  className="d-flex align-items-center    nav-link"
+                >
+                  <FaHome className="me-1 fs-5" />
+                  home
+                </Link>
+                <Link to="/add" className="d-flex align-items-center nav-link">
+                  <PiNotepadDuotone className="me-1 fs-5" />
+                  add recipe
+                </Link>
+                <Link to="/show" className="d-flex align-items-center nav-link">
+                  <FaClipboardList className="me-1 fs-5" /> all recipes
+                </Link>
+                <Link to="/pricing" className="d-flex align-items-center nav-link">
+                  <MdOutlinePriceChange className="me-1 fs-5" /> Pricing
+                </Link>
+                <Link to="/login" className="d-flex align-items-center nav-link">
+                  <MdAccountBox className="me-1 fs-5" /> login
+                </Link>
+              </Nav>
+            </Col>
+          </Container>
+        </Navbar>
+      </header>
+      {/* <Banner /> */}
+    </>
+  );
+};
+
+export default home;
